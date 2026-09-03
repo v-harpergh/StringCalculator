c3929bb14bf7934f71d9fbce051151375ed47b4a
victor <tv58246@mail.harpercollege.edu>
2026-09-03 10:38:33 -0500

    basic version

--- StringCalculator/StringCalculator.cpp
+++ StringCalculator/StringCalculator.cpp
@@ -3,9 +3,65 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> delimiters{};
+// I could just use free functions from STL but I think it ruins the point of the assignment
+bool isdelimiter(std::vector<char>& delimiters, const char c) {
+	for (const auto& v : delimiters) {
+		if (c == v) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int64_t parse_input(std::vector<char>& delimiters, std::string_view input) {
+	int64_t result = 0;
+	std::vector<int64_t> numbers{};
+	std::string buffer{};
+
+	for (const auto c : input) {
+		if (isdelimiter(delimiters, c)) {
+			if (!buffer.empty()) {
+				numbers.push_back(std::stoi(buffer));
+			}
+			buffer = "";
+		}
+		else {
+			buffer += c;
+		}
+	}
+
+	if (!buffer.empty()) {
+		numbers.push_back(std::stoi(buffer));
+	}
+
+	for (const auto& n : numbers) {
+		result += n;
+	}
+
+	return result;
+}
 
 int main()
 {
+	std::vector<char> delimiters = { ',', ';' };
+	std::string input{};
 
+	std::cout << "Enter numbers separated by [";
+
+	for (const auto& d : delimiters) {
+		std::cout << d << "";
+	}
+
+	std::cout << "]\n";
+
+	std::cin >> input;
+
+	if (std::cin.fail()) {
+		std::cout << "std::cin failed";
+		return 1;
+	}
+
+	int64_t result = parse_input(delimiters, input);
+
+	std::cout << result << "\n";
 }
